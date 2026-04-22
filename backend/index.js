3237266e3f953730287b4047dd2ec7d3389555a4
SanjayChoodamani <sanjaychoodamani13@gmail.com>
2026-04-22 12:58:23 +0530

    update port

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ import authRoutes from "./routes/auth.routes.js"; // Importing auth routes
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware for JSON parsing
 app.use(express.json());
